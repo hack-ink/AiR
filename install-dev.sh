ee4ab86de5467acd01103272a629e4dc01ab9fe5
Xavier Lau <xavier@inv.cafe>
2024-07-11 11:21:03 +0800

    Bump deps

---
+++ install-dev.sh
@@ -0,0 +1,2 @@
+codesign --deep --force --verify --verbose --sign "Xavier Lau" target/release/bundle/osx/AiR.app
+rm -rf /Applications/AiR.app && cp -r target/release/bundle/osx/AiR.app /Applications/AiR.app
