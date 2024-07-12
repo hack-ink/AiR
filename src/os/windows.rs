2967a382445b0600a0c40b860bfe33a6ae85c7a3
Xavier Lau <xavier@inv.cafe>
2024-07-13 02:09:23 +0800

    Fix build

--- src/os/windows.rs
+++ src/os/windows.rs
@@ -1,1 +1,12 @@
+// self
+use super::*;
 
+impl Os {
+	// #[deprecated(note = "use `ViewportCommand` instead")]
+	pub fn hide() {}
+
+	// #[deprecated(note = "use `ViewportCommand` instead")]
+	pub fn unhide() {}
+
+	pub fn set_move_to_active_space() {}
+}
