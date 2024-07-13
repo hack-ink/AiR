d617c7e03582c4119889ad778a54d1bada06892e
Xavier Lau <xavier@inv.cafe>
2024-07-14 03:52:52 +0800

    Cache macOS window handle

--- src/component/os.rs
+++ src/component/os.rs
@@ -4,12 +4,16 @@
 
 #[derive(Debug)]
 pub struct Os {
+	#[cfg(target_os = "macos")]
+	nsra: objc2::rc::Retained<objc2_app_kit::NSRunningApplication>,
 	#[cfg(target_os = "windows")]
 	hwnd: *mut winapi::shared::windef::HWND__,
 }
 impl Os {
 	pub fn new() -> Self {
 		Self {
+			#[cfg(target_os = "macos")]
+			nsra: Self::get_nsra(),
 			#[cfg(target_os = "windows")]
 			hwnd: Self::get_hwnd(),
 		}
--- src/component/os/macos.rs
+++ src/component/os/macos.rs
@@ -1,21 +1,24 @@
 // crates.io
+use objc2::rc::Retained;
 use objc2_app_kit::{NSApplication, NSRunningApplication, NSWindowCollectionBehavior};
 use objc2_foundation::MainThreadMarker;
 // self
 use super::*;
 
 impl Os {
-	// #[deprecated(note = "use `ViewportCommand` instead")]
+	pub fn get_nsra() -> Retained<NSRunningApplication> {
+		unsafe { NSRunningApplication::currentApplication() }
+	}
+
 	pub fn hide(&self) {
 		unsafe {
-			NSRunningApplication::currentApplication().hide();
+			self.nsra.hide();
 		}
 	}
 
-	// #[deprecated(note = "use `ViewportCommand` instead")]
 	pub fn unhide(&self) {
 		unsafe {
-			NSRunningApplication::currentApplication().unhide();
+			self.nsra.unhide();
 		}
 	}
 
@@ -35,57 +38,4 @@ impl Os {
 				.setCollectionBehavior(NSWindowCollectionBehavior::MoveToActiveSpace);
 		}
 	}
-
-	// fn get_selected_text() {
-	// 	// crates.io
-	// 	use accessibility::{AXAttribute, AXUIElement, AXUIElementAttributes};
-	// 	use accessibility_sys::{
-	// 		kAXFocusedUIElementAttribute, kAXFocusedWindowAttribute, kAXSelectedTextAttribute,
-	// 	};
-	// 	use core_foundation::{
-	// 		base::{CFType, ToVoid},
-	// 		string::CFString,
-	// 	};
-	//
-	// 	fn selected_text() -> Option<String> {
-	// 		fn attr(attr: &'static str) -> AXAttribute<CFType> {
-	// 			AXAttribute::new(&CFString::from_static_string(attr))
-	// 		}
-	//
-	// 		fn try_get_focus_element(ax_ui_element: &AXUIElement) -> Option<AXUIElement> {
-	// 			if let Ok(e) = ax_ui_element.attribute(&attr(kAXFocusedUIElementAttribute)) {
-	// 				return e.downcast_into();
-	// 			}
-	//
-	// 			ax_ui_element
-	// 				.children()
-	// 				.ok()
-	// 				.and_then(|es| es.iter().find_map(|e| try_get_focus_element(&e)))
-	// 		}
-	//
-	// 		let pid = unsafe {
-	// 			let workspace = NSWorkspace::sharedWorkspace();
-	// 			let app = workspace.frontmostApplication()?;
-	//
-	// 			app.processIdentifier()
-	// 		};
-	// 		let root = AXUIElement::application(pid);
-	// 		let window = root
-	// 			.attribute(&attr(kAXFocusedWindowAttribute))
-	// 			.unwrap()
-	// 			.downcast_into::<AXUIElement>()?;
-	//
-	// 		tracing::debug!("window: {window:?}");
-	//
-	// 		let element = try_get_focus_element(&window)?;
-	//
-	// 		tracing::debug!("element: {window:?}");
-	//
-	// 		element
-	// 			.attribute(&attr(kAXSelectedTextAttribute))
-	// 			.ok()
-	// 			.and_then(|t| t.downcast_into::<CFString>())
-	// 			.map(|t| t.to_string())
-	// 	}
-	// }
 }
