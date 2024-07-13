bcde84a2a88f19d2efe30f0a759ee21540fcea71
Xavier Lau <xavier@inv.cafe>
2024-07-13 12:43:55 +0800

    Support Windows virtual key codes

--- src/component/keyboard.rs
+++ src/component/keyboard.rs
@@ -60,10 +60,9 @@ impl FromStr for Keys {
 	}
 }
 
+// We can't use [`enigo::Key::Unicode`], it will cause panic.
+// Don't know why, maybe that can only be used in main thread.
 fn key_of(key: char) -> Result<Key> {
-	// We can't use [`enigo::Key::Unicode`] in macOS, it will cause panic.
-	// Don't know why, maybe that can only be used in main thread.
-	//
 	// TODO: create a `CGKeyCode` table for macOS in `build.rs`.
 	// Currently, we only support limited keys on macOS from:
 	// https://eastmanreference.com/complete-list-of-applescript-key-codes.
@@ -118,7 +117,62 @@ fn key_of(key: char) -> Result<Key> {
 		'`' => 50,
 		_ => return Err(Error::UnsupportedKey(key.to_string())),
 	});
-	#[cfg(not(target_os = "macos"))]
+	// TODO: create a `Virtual-Key Codes` table for Windows in `build.rs`.
+	// Currently, we only support limited keys on Windows from:
+	// https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes.
+	#[cfg(target_os = "windows")]
+	let k = Key::Other(match key {
+		'0' => 0x30,
+		'1' => 0x31,
+		'2' => 0x32,
+		'3' => 0x33,
+		'4' => 0x34,
+		'5' => 0x35,
+		'6' => 0x36,
+		'7' => 0x37,
+		'8' => 0x38,
+		'9' => 0x39,
+		'A' => 0x41,
+		'B' => 0x42,
+		'C' => 0x43,
+		'D' => 0x44,
+		'E' => 0x45,
+		'F' => 0x46,
+		'G' => 0x47,
+		'H' => 0x48,
+		'I' => 0x49,
+		'J' => 0x4A,
+		'K' => 0x4B,
+		'L' => 0x4C,
+		'M' => 0x4D,
+		'N' => 0x4E,
+		'O' => 0x4F,
+		'P' => 0x50,
+		'Q' => 0x51,
+		'R' => 0x52,
+		'S' => 0x53,
+		'T' => 0x54,
+		'U' => 0x55,
+		'V' => 0x56,
+		'W' => 0x57,
+		'X' => 0x58,
+		'Y' => 0x59,
+		'Z' => 0x5A,
+		'-' => 0xBD,
+		'=' => 0xBB,
+		'[' => 0xDB,
+		']' => 0xDD,
+		'\\' => 0xDC,
+		';' => 0xBA,
+		'\'' => 0xDE,
+		',' => 0xBC,
+		'.' => 0xBE,
+		'/' => 0xBF,
+		'`' => 0xC0,
+		_ => return Err(Error::UnsupportedKey(key.to_string())),
+	});
+
+	#[cfg(all(unix, not(target_os = "macos")))]
 	let k = Key::Unicode(key);
 
 	Ok(k)
